bfebc4ea7724bd455462a4e1510080e2a85e484d
RKing <rosalyn.kingsmill@gmail.com>
2016-05-16 12:09:53 -0400

    Initial Commit

---
+++ ScrollViewImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrollViewImage
+//
+//  Created by Rosalyn Kingsmill on 2016-05-16.
+//  Copyright © 2016 Rosalyn Kingsmill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
