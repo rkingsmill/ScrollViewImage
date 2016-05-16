3ff1fdb8812ef5b40688c27baa130b8301deb895
RKing <rosalyn.kingsmill@gmail.com>
2016-05-16 13:21:39 -0400

    Part 1

--- ScrollViewImage/ViewController.m
+++ ScrollViewImage/ViewController.m
@@ -8,7 +8,12 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UIScrollViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView1;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView2;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView3;
 
 @end
 
@@ -16,6 +21,11 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    
+   self.scrollView.delegate = self;
+    self.scrollView.translatesAutoresizingMaskIntoConstraints = YES;
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
